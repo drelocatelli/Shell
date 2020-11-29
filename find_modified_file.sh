5c57896e18eadcd8f293996fd2aaec2ed91f8fec
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-11-28 23:37:07 -0300

    Update find_modified_file.sh

--- find_modified_file.sh
+++ find_modified_file.sh
@@ -1,3 +1,4 @@
+# LISTA OU DELETA ARQUIVOS COM X TEMPO DE MODIFICAÇÃO
 # Exemplo de como utilizar :::: sh find_modified_file.sh "Pasta" "n [years|days|month|minutes] [ago]" [list|delete]
 echo "-----------------------------" && date && echo "-----------------------------"
 folder_path="$HOME/$1"
