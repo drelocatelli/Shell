54660aae9c5d24e91ea4b07560df0658998118c7
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-11-28 23:28:35 -0300

    Add files via upload

---
+++ find_modified_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Exemplo de como utilizar :::: sh find_modified_time_and_remove.sh "Pasta" "n [years|days|month|minutes] [ago]" [list|delete]
+echo "-----------------------------" && date && echo "-----------------------------"
+folder_path="$HOME/$1"
+if [ "$3" = "list" ]
+then
+    echo "Listando arquivos com '$2'."
+    find "$folder_path" -not -path "$folder_path" -type f -newermt "$2" -exec ls -lhag --time-style=+"| %d %b %Y %H:%M:%S |" {}  \;
+elif [ "$3" = "delete" ]
+then
+    echo "Removendo arquivos com '$2'."
+    find "$folder_path" -not -path "$folder_path" -type f -newermt "$2" -exec rm -rv {} \;
+fi
+
+cd ~
