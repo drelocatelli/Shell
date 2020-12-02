651614c6042ca40b6c7eb8d2cdf5574b83a2a0c3
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-12-02 18:21:59 -0300

    Update find_modified_file.sh

--- find_modified_file.sh
+++ find_modified_file.sh
@@ -1,10 +1,10 @@
 # LISTA OU DELETA ARQUIVOS COM X TEMPO DE MODIFICAÇÃO
 # Exemplo de como utilizar :::: sh find_modified_file.sh "Pasta" "[n] [years|days|month|minutes] [ago]" [list|delete|custom] [custom_command]
 # sh find_modified_file.sh Downloads "1 minute ago" list
-echo "-----------------------------" && date && echo "-----------------------------"
 folder_path="$HOME/$1"
 if [ "$3" = "list" ]
 then
+    echo "-----------------------------" && date && echo "-----------------------------"
     echo "Listando arquivos com '$2'."
     find "$folder_path" -not -path "$folder_path" -type f -newermt "$2" -exec ls -lhag --time-style=+"| %d %b %Y %H:%M:%S |" {}  \;
 elif [ "$3" = "delete" ]
